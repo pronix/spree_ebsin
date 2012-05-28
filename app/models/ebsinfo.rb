dd06e982d5ede1ba35ee60b425d6e8acde0dd340
abhishek <bigbeliever@gmail.com>
2012-05-28 16:08:30 +0530

    Added some payment processing logic in Ebsinfo

--- app/models/ebsinfo.rb
+++ app/models/ebsinfo.rb
@@ -1,2 +1,29 @@
 class Ebsinfo < ActiveRecord::Base
+
+  def actions
+    %w{mark_as_captured void}
+  end
+  
+  # Indicates whether its possible to capture the payment
+  def can_mark_as_captured?(payment)
+    ['checkout', 'pending'].include?(payment.state)
+  end
+  
+  # Indicates whether its possible to void the payment.
+  def can_void?(payment)
+    payment.state != 'void'
+  end
+  
+  def mark_as_captured(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.complete
+    true
+  end
+  
+  def void(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.void
+    true
+  end
+
 end
