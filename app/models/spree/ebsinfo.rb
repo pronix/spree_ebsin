9a605c8d74593e9bcd1779761d9288fd7d2140ee
Surya Tripathi <raj.surya19@gmail.com>
2013-08-01 14:08:48 +0530

    last name

--- app/models/spree/ebsinfo.rb
+++ app/models/spree/ebsinfo.rb
@@ -1,10 +1,10 @@
 module Spree
   class Ebsinfo < ActiveRecord::Base
-    attr_accessible :first_name, :TransactionId, :PaymentId, :amount, :order_id
+    attr_accessible :first_name, :last_name, :TransactionId, :PaymentId, :amount, :order_id
     self.table_name = :ebsinfos
 
     has_many :payments, :as => :source
-    
+
     def actions
       %w{mark_as_captured void}
     end
